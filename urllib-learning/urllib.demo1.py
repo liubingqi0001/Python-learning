91fa77cda244485092e7de1d4c0c1dc098062de5
wendaolbq <121316924@qq.com>
2019-07-18 01:26:59 +0800

    台式机初次调试，成功运行

--- urllib-learning/urllib.demo1.py
+++ urllib-learning/urllib.demo1.py
@@ -6,5 +6,5 @@ print(resp.readline())
 print(resp.getcode())
 
 #urlretrieve函数
-#request.urlretrieve('http://www.baidu.com/','baidu.html')
+request.urlretrieve('http://www.baidu.com/','baidu.html')
 request.urlretrieve('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563357876488&di=a01e3f8a3eeb2cf4808ce4c09c2e4d2f&imgtype=0&src=http%3A%2F%2Fpic.9ht.com%2Fup%2F2017-4%2F14910172822196790.jpg', 'DM5.jpg')
