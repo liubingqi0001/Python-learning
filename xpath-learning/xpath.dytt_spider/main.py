b8bd37be9f5e96d63e5706fd13ed6f5020d3c1d7
wendaolbq <121316924@qq.com>
2019-08-05 23:55:32 +0800

    电影天堂 爬虫代码 未完待续

--- xpath-learning/xpath.dytt_spider/main.py
+++ xpath-learning/xpath.dytt_spider/main.py
@@ -1,19 +1,56 @@
 from lxml import etree
 import requests
 BASE_DOMAIN = 'http://dytt8.net'
+HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    }
 
-url = 'https://dytt8.net/html/gndy/dyzz/list_23_1.html'
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-}
-response = requests.get(url,headers=headers)
-# print(response.text)
-# 注意此处涉及上一讲中提到的text和content的区别，所以直接用text打印会出现筹码
-# requests库，为谁会使用自己猜测的编码方式，将抓取下来的网页进行解码，然后存储到text属性上
-# 在电影天堂地的网页中，因为编码方式，requests库猜错了，所以就会产生乱码
-# print(response.content.decode('gbk'))   #查询编码方式见笔记，此处是可以正确打印出来的
-text = response.content.decode('gbk')
-html = etree.HTML(text)
-detail_urls =  html.xpath("//table[@class = 'tbspan']//a/@href")
-for detail_url in detail_urls:
-    print(BASE_DOMAIN + detail_url)
+
+def get_detail_urls(url):
+    # url = 'https://dytt8.net/html/gndy/dyzz/list_23_1.html'
+
+    response = requests.get(url, headers=HEADERS)
+    # print(response.text)
+    # 注意此处涉及上一讲中提到的text和content的区别，所以直接用text打印会出现筹码
+    # requests库，为谁会使用自己猜测的编码方式，将抓取下来的网页进行解码，然后存储到text属性上
+    # 在电影天堂地的网页中，因为编码方式，requests库猜错了，所以就会产生乱码
+    # print(response.content.decode('gbk'))   #查询编码方式见笔记，此处是可以正确打印出来的
+    # print(response.encoding)     #此代码用于查看默认的编码方式
+
+    # text = response.content.decode('gbk')
+    text = response.text
+    # text = response.text.encode().decode()
+    html = etree.HTML(text)
+    detail_urls = html.xpath("//table[@class = 'tbspan']//a/@href")
+    #以下的map函数等价于以下注释的代码
+    # def abc(url):
+    #     return BASE_DOMAIN+url
+    # index = 0
+    # for detail_url in detail_urls:
+    #     detail_url=abc(detail_url)
+    #     detail_urls[index]=detail_url
+    #     index += 1
+
+    detail_urls = map(lambda url:BASE_DOMAIN+url,detail_urls)
+    return detail_urls
+
+    # for detail_url in detail_urls:
+    #     print(BASE_DOMAIN + detail_url)
+
+def parse_detail_page(url):
+    pass
+
+def spider():
+    base_url = 'https://dytt8.net/html/gndy/dyzz/list_23_{}.html'
+    for x in range(1,8):
+        print('x'*30)
+        print(x)
+        print('x'*30)
+        url = base_url.format(x)
+        # print(url)
+        detail_urls = get_detail_urls(url)
+        for detail_url in detail_urls:
+            print(detail_url)
+
+if __name__ == '__main__':
+    spider()
